96b219063dea9253e101d98aec6e2de5d3ff877f
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2024-03-07 08:29:22 +0100

    Add docstrings for formatter

--- pkg/logger/formatter/formatter.go
+++ pkg/logger/formatter/formatter.go
@@ -1,3 +1,5 @@
+// Package formatter contains formatter that interpolates template strings and
+// formats them.
 package formatter
 
 import (
@@ -26,44 +28,53 @@ var logLevelColors = map[loglevel.LogLevel]string{
 // Reset color
 const resetColor = "\033[0m"
 
+// Interface represents interface that shall be satisfied by Formatter.
 type Interface interface {
 	Template() string
 	Format(message string, loggerName string, level loglevel.LogLevel, colored bool) string
 }
 
+// Formatter struct that contains necessary for the formatting fields.
 type Formatter struct {
 	template string
 }
 
+// New create a new instance of the Formatter.
 func New(template string) *Formatter {
 	return &Formatter{template: template}
 }
 
+// IsEqual checks that two formatters are the same and returns result of the
+// comparison.
 func (formatter *Formatter) IsEqual(anotherFormatter *Formatter) bool {
 	return formatter.template == anotherFormatter.template
 }
 
+// EvaluatePreset evaluates pre-defined set of formatting options and returns map
+// with mapping of the option to interpolated value.
 func (formatter *Formatter) EvaluatePreset(message string, loggerName string, level loglevel.LogLevel) map[string]string {
 	_, functionName, functionLine, _ := runtime.Caller(2)
 	var presets = map[string]string{
-		"%(name)":      loggerName,
-		"%(message)":   message,
-		"%(time)":      time.Now().Format(time.TimeOnly),
-		"%(date)":      time.Now().Format(time.DateOnly),
-		"%(isotime)":   time.Now().Format(time.RFC3339),
-		"%(timestamp)": strconv.FormatInt(time.Now().Unix(), 10),
-		"%(level)":     level.String(),
-		"%(levelnr)":   strconv.Itoa(level.DigitRepresentation()),
-		"%(fname)":     functionName,
-		"%(fline)":     strconv.Itoa(functionLine),
+		"%(name)":      loggerName,                                // Logger name
+		"%(message)":   message,                                   // Logged message
+		"%(time)":      time.Now().Format(time.TimeOnly),          // Current time (format: HH:MM:ss)
+		"%(date)":      time.Now().Format(time.DateOnly),          // Current date (format: yyyy-mm-dd)
+		"%(isotime)":   time.Now().Format(time.RFC3339),           // Current date and time (format: yyyy-mm-ddTHH:MM:ssGMT)
+		"%(timestamp)": strconv.FormatInt(time.Now().Unix(), 10),  // Current timestamp
+		"%(level)":     level.String(),                            // Logging level name
+		"%(levelnr)":   strconv.Itoa(level.DigitRepresentation()), // Logging level number
+		"%(fname)":     functionName,                              // Name of the function from which logger has been called
+		"%(fline)":     strconv.Itoa(functionLine),                // Line number from which logger has been called
 	}
 	return presets
 }
 
+// Template returns template string used by formatter.
 func (formatter *Formatter) Template() string {
 	return formatter.template
 }
 
+// Format formats provided message template to the interpolated string.
 func (formatter *Formatter) Format(message string, loggerName string, level loglevel.LogLevel, colored bool) string {
 	var presets = formatter.EvaluatePreset(message, loggerName, level)
 
