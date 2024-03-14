d636a467abcb5eb071cd4711ac8843afe294f885
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2024-03-14 19:27:23 +0100

    Update handler to support from and to levels

--- pkg/logger/handler/handler.go
+++ pkg/logger/handler/handler.go
@@ -17,59 +17,84 @@ var osStderr = os.Stderr
 
 // Interface represents interface that shall be satisfied by Handler.
 type Interface interface {
-	Level() loglevel.LogLevel
-	SetLevel(level loglevel.LogLevel)
+	Writer() io.Writer
+	FromLevel() loglevel.LogLevel
+	SetFromLevel(fromLevel loglevel.LogLevel)
+	ToLevel() loglevel.LogLevel
+	SetToLevel(toLevel loglevel.LogLevel)
 	Formatter() formatter.Interface
 	Write(logName string, level loglevel.LogLevel, message string, parameters ...any)
 }
 
 // Handler struct contains information where it shall write log message, how to
-// format them and their log level.
+// format them and their log fromLevel.
 type Handler struct {
-	level                     loglevel.LogLevel
+	fromLevel                 loglevel.LogLevel
+	toLevel                   loglevel.LogLevel
 	formatter                 formatter.Interface
 	writer                    io.Writer
-	errorWriter               io.Writer
 	consoleSupportsANSIColors func() bool
 }
 
 // New create a new instance of the Handler.
-func New(level loglevel.LogLevel, newFormatter formatter.Interface, writer io.Writer, errorWriter io.Writer) *Handler {
+func New(fromLevel loglevel.LogLevel, toLevel loglevel.LogLevel, newFormatter formatter.Interface, writer io.Writer) *Handler {
 	return &Handler{
-		level:                     level,
+		fromLevel:                 fromLevel,
+		toLevel:                   toLevel,
 		formatter:                 newFormatter,
 		writer:                    writer,
-		errorWriter:               errorWriter,
 		consoleSupportsANSIColors: consoleSupportsANSIColors,
 	}
 }
 
 // NewConsoleHandler create a new instance of the Handler that writes log
-// messages to the os.Stdout and os.Stderr respectively.
-func NewConsoleHandler(level loglevel.LogLevel, newFormatter formatter.Interface) *Handler {
-	return New(level, newFormatter, osStdout, osStderr)
+// messages to the os.Stdout.
+func NewConsoleHandler(fromLevel loglevel.LogLevel, toLevel loglevel.LogLevel, newFormatter formatter.Interface) *Handler {
+	return New(fromLevel, toLevel, newFormatter, osStdout)
+}
+
+// NewConsoleErrorHandler create a new instance of the Handler that writes log
+// messages to the os.Stderr.
+func NewConsoleErrorHandler(fromLevel loglevel.LogLevel, toLevel loglevel.LogLevel, newFormatter formatter.Interface) *Handler {
+	return New(fromLevel, toLevel, newFormatter, osStderr)
 }
 
 // NewFileHandler creates a new instance of the Handler that writes log message
 // to the log file.
-func NewFileHandler(level loglevel.LogLevel, newFormatter formatter.Interface, file string) *Handler {
+func NewFileHandler(fromLevel loglevel.LogLevel, toLevel loglevel.LogLevel, newFormatter formatter.Interface, file string) *Handler {
 	writer, err := osOpenFile(file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
 	if err != nil {
 		fmt.Println(err)
+		return nil
 	}
 
-	return New(level, newFormatter, writer, writer)
+	return New(fromLevel, toLevel, newFormatter, writer)
 }
 
-// Level returns log level of the Handler.
-func (handler *Handler) Level() loglevel.LogLevel {
-	return handler.level
+// Writer returns writer of the Handler.
+func (handler *Handler) Writer() io.Writer {
+	return handler.writer
 }
 
-// SetLevel sets a new log level for the Handler.
-func (handler *Handler) SetLevel(level loglevel.LogLevel) {
-	handler.level = level
+// FromLevel returns log fromLevel of the Handler.
+func (handler *Handler) FromLevel() loglevel.LogLevel {
+	return handler.fromLevel
+}
+
+// SetFromLevel sets a new log fromLevel for the Handler.
+func (handler *Handler) SetFromLevel(fromLevel loglevel.LogLevel) {
+	handler.fromLevel = fromLevel
+}
+
+// ToLevel returns log toLevel of the Handler.
+func (handler *Handler) ToLevel() loglevel.LogLevel {
+	return handler.toLevel
+}
+
+// SetToLevel sets a new log toLevel for the Handler.
+func (handler *Handler) SetToLevel(toLevel loglevel.LogLevel) {
+	handler.toLevel = toLevel
 }
 
 // Formatter returns formatter.Interface used by the Handler.
@@ -79,23 +104,21 @@ func (handler *Handler) Formatter() formatter.Interface {
 
 // Write writes log message to the defined by the Handler writer.
 func (handler *Handler) Write(logName string, level loglevel.LogLevel, message string, parameters ...any) {
-	formattedMessage := fmt.Sprintf(message, parameters...)
-
-	writer := handler.writer
-
-	if level >= loglevel.Error {
-		writer = handler.errorWriter
+	if level.DigitRepresentation() < handler.fromLevel.DigitRepresentation() || level.DigitRepresentation() > handler.toLevel.DigitRepresentation() {
+		return
 	}
+
+	formattedMessage := fmt.Sprintf(message, parameters...)
 
 	var colored = false
 
-	if handler.consoleSupportsANSIColors() && (writer == osStdout || writer == osStderr) {
+	if handler.consoleSupportsANSIColors() && (handler.writer == osStdout || handler.writer == osStderr) {
 		colored = true
 	}
 
 	log := handler.formatter.Format(formattedMessage, logName, level, colored)
 
-	if _, err := writer.Write([]byte(log)); err != nil {
+	if _, err := handler.writer.Write([]byte(log)); err != nil {
 		fmt.Println(err)
 	}
 }
