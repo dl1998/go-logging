37a4bebd283e4566e9e3dcb3dec86ddebc2edf24
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2024-04-01 02:00:45 +0200

    Refactor structuredlogger, move part of the logic to common package

--- pkg/structuredlogger/structuredlogger.go
+++ pkg/structuredlogger/structuredlogger.go
@@ -5,7 +5,6 @@ import (
 	"github.com/dl1998/go-logging/pkg/common/level"
 	"github.com/dl1998/go-logging/pkg/structuredlogger/formatter"
 	"github.com/dl1998/go-logging/pkg/structuredlogger/handler"
-	"github.com/dl1998/go-logging/pkg/structuredlogger/logrecord"
 	"time"
 )
 
@@ -16,78 +15,6 @@ var template map[string]string
 
 func init() {
 	Configure(NewConfiguration())
-}
-
-// baseLoggerInterface defines low level logging interface.
-type baseLoggerInterface interface {
-	Log(level level.Level, parameters ...any)
-	Name() string
-	SetName(name string)
-	Handlers() []handler.Interface
-	AddHandler(handlerInterface handler.Interface)
-	RemoveHandler(handlerInterface handler.Interface)
-}
-
-// baseLogger struct contains basic fields for the logger.
-type baseLogger struct {
-	name       string
-	timeFormat string
-	handlers   []handler.Interface
-}
-
-// Log logs interpolated message with the provided level.Level.
-func (logger *baseLogger) Log(logLevel level.Level, parameters ...any) {
-	var parametersMap = make(map[string]interface{})
-	parametersCount := len(parameters)
-
-	if parametersCount == 1 {
-		parametersMap = parameters[0].(map[string]interface{})
-	} else if parametersCount > 1 {
-		if parametersCount%2 != 0 {
-			parametersCount--
-		}
-		for index := 0; index < parametersCount; index += 2 {
-			parametersMap[parameters[index].(string)] = parameters[index+1]
-		}
-	}
-
-	logRecord := logrecord.New(logger.name, logLevel, logger.timeFormat, parametersMap, 3)
-
-	for _, registeredHandler := range logger.handlers {
-		registeredHandler.Write(logRecord)
-	}
-}
-
-// Name return baseLogger name.
-func (logger *baseLogger) Name() string {
-	return logger.name
-}
-
-// SetName sets a new name for the baseLogger.
-func (logger *baseLogger) SetName(name string) {
-	logger.name = name
-}
-
-// Handlers returns a list of the registered handler.Interface objects for the
-// baseLogger.
-func (logger *baseLogger) Handlers() []handler.Interface {
-	return logger.handlers
-}
-
-// AddHandler register a new handler.Interface for the baseLogger.
-func (logger *baseLogger) AddHandler(handlerInterface handler.Interface) {
-	logger.handlers = append(logger.handlers, handlerInterface)
-}
-
-// RemoveHandler removes a handler.Interface from the baseLogger handlers.
-func (logger *baseLogger) RemoveHandler(handlerInterface handler.Interface) {
-	newSlice := make([]handler.Interface, 0)
-	for _, element := range logger.handlers {
-		if element != handlerInterface {
-			newSlice = append(newSlice, element)
-		}
-	}
-	logger.handlers = newSlice
 }
 
 // Interface represents interface that shall be satisfied by Logger.
